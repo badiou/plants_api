9c9de604012bb88f017e47bfd200c8442614c3ba
badiou <ourobadiou@gmail.com>
2021-04-09 09:35:38 +0000

    Write a read me for Plants_API

--- test_flaskr.py
+++ test_flaskr.py
@@ -53,20 +53,20 @@ class PlanttestCase(unittest.TestCase):
         res = self.client().get('/plants')
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['current_plants'], True)
-        self.assertTruelen(data['current_plants'])
+        self.assertTrue(data['current_plants'])
+        self.assertTrue(data['current_plants'])
 
     def test_delete_plant(self):
-        res=self.client().delete('/plants/4')
+        res=self.client().delete('/plants/101')
         data=json.loads(res.data)
-        plant=Plant.query.filter(Plant.id == 11).one_or_none()
+        plant=Plant.query.filter(Plant.id == 101).one_or_none()
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'], 4)
+        self.assertEqual(data['deleted'], 101)
         self.assertTrue(data['plants'], True)
         self.assertTrue(data['totals_plants'])
 
-    def test_delete_plant_which_does_not_exist(self):
+    def test_plant_which_does_not_exist(self):
         res=self.client().delete('/plants/1000')
         data=json.loads(res.data)
         plant=Plant.query.filter(Plant.id == 1000).one_or_none()
@@ -82,7 +82,7 @@ class PlanttestCase(unittest.TestCase):
         self.assertTrue(data['success'], True)
         self.assertTrue(data['created'])
         self.assertTrue(data['plants'])
-        self.assertEqual(len(data['totals_plants']))
+        self.assertEqual(len(data['plants']))
 
     def test_404_requesting_beyond_the_value_page(self):
         res=self.client().get('/plants?page=1000')
@@ -99,12 +99,20 @@ class PlanttestCase(unittest.TestCase):
         self.assertEqual(data['message'], 'Not found')
     
     def test_update_plant_primary_color(self):
-        res=self.client().patch('/plants/6',json={'primary_color':'Gray'})
+        res=self.client().patch('/plants/102',json={'primary_color':'Gray'})
         data=json.loads(res.data)
-        plant=Plant.query.filter(Plant.id == 6).one_or_none()
+        plant=Plant.query.filter(Plant.id == 102).one_or_none()
         self.assertEqual(res.status_code,200)
         self.assertEqual(data['success'],True)
         self.assertEqual(plant.format()['primary_color'],'Gray')
+
+    def test_get_plant_search_with_result(self):
+        res=self.client().post('/plants',json={'search':'Baobab'})
+        self.assertEqual(res.status_code,200)
+        self.assertEqual(data['success'],True)
+        self.assertEqual(len(data['total_plants']))
+        self.assertEqual(len(data['plants']),4)
+
 
 # # Make the tests conveniently executable
 if __name__ == "__main__":
